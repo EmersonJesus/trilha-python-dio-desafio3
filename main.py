186d49bcddebb502076e6f3c4f38128ce6e749e9
Emerson Jesus <Emersoncruz73@hotmail.com>
2024-04-17 20:49:57 -0300

    add class"Cliente"

--- main.py
+++ main.py
@@ -17,3 +17,33 @@ class PessoaFisica(Cliente):
         self.data_nascimento = data_nascimento
         self.cpf = cpf
 
+class Conta:
+    def __init__(self, numero, cliente):
+        self._saldo = 0
+        self._numero = numero
+        self._agencia = "0001"
+        self._cliente = cliente
+
+        @classmethod
+        def nova_conta(cls, cliente, numero):
+            return cls(numero, cliente)
+        
+        @property
+        def saldo(self):
+            return self._saldo
+        
+        @property
+        def numero(self):
+            return self._numero
+        
+        @property
+        def agencia(self):
+            return self._agencia
+        
+        @property
+        def cliente(self):
+            return self._cliente
+        
+        @property
+        def historico(self):
+            return self._historico
