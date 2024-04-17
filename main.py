bc895d24f4bbd6945cfe8b598fde0b9c7cf22736
Emerson Jesus <Emersoncruz73@hotmail.com>
2024-04-17 20:43:19 -0300

    add class "PessoaFisica"

--- main.py
+++ main.py
@@ -9,3 +9,11 @@ class Cliente:
 
     def adicionar_contas(self, conta):
         pass
+
+class PessoaFisica(Cliente):
+    def __init__(self, nome, data_nascimento, cpf, endereco):
+        super().__init__(endereco)
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        self.cpf = cpf
+
